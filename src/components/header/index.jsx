545e9c56b4c84285bb9ee947bc1791a1340d617d
sulthan <user@users-MacBook-Pro.local>
2025-05-19 18:52:23 +0700

    responsif

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,41 @@
-import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import assetImg from "../../assets/asset-logo.png";
 import "./index.scss";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <div className="con-header">
-      <Flex className="header">
-        <img src={assetImg} />
-        <Flex className="navbar-menu">
+      <Flex className="header" justify="space-between" align="center">
+        <img src={assetImg} className="logo" />
+
+        <Flex className="navbar-menu desktop-nav">
           <p>Beranda</p>
           <p>Produk</p>
           <p>Harga</p>
           <p className="kontak">Kontak</p>
         </Flex>
+
+        <IconButton
+          aria-label="Toggle Menu"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          display={{ base: "block", md: "none" }}
+          onClick={toggleMenu}
+          className="hamburger-icon"
+        />
       </Flex>
+      {isOpen && (
+        <Flex direction="column" className="mobile-nav">
+          <p>Beranda</p>
+          <p>Produk</p>
+          <p>Harga</p>
+          <p className="kontak">Kontak</p>
+        </Flex>
+      )}
     </div>
   );
 }
